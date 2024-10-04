21a7c0217fcc219766584c0308e95b871e8cbbfb
matheusassis08 <matheusassis331@gmail.com>
2024-10-03 23:13:52 -0300

    Fiz e documentei a classe gerente

--- Academia/src/academia/Gerente.java
+++ Academia/src/academia/Gerente.java
@@ -4,10 +4,41 @@
  */
 package academia;
 
+
 /**
- *
- * @author Leona
+ * A classe Gerente representa um gerente que trabalha na academia.
+ * Ela herda os atributos e comportamentos da classe abstrata Pessoa.
  */
-public class Gerente {
-    
+public class Gerente extends Pessoa {
+
+    private String departamento;
+
+    /**
+     * Construtor da classe Gerente.
+     * 
+     * @param nome       O nome completo do gerente.
+     * @param cpf        O CPF do gerente.
+     * @param endereco   O endereço residencial do gerente.
+     * @param telefone   O número de telefone do gerente.
+     * @param email      O endereço de email do gerente.
+     * @param departamento O departamento onde o gerente atua.
+     */
+    public Gerente(String nome, String cpf, String endereco, String telefone, String email, String departamento) {
+        super(nome, cpf, endereco, telefone, email); // Chama o construtor da superclasse Pessoa
+        this.departamento = departamento;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    @Override
+    public String toString() {
+        return "Gerente -> " + super.toString() + ", Departamento: " + departamento;
+    }
 }
+
