b68559b295d59703ddbd338701038ac116da8862
matheusassis08 <matheusassis331@gmail.com>
2024-10-03 21:23:47 -0300

    Coloquei métodos classe pessoa

--- Academia/src/academia/Pessoa.java
+++ Academia/src/academia/Pessoa.java
@@ -4,10 +4,69 @@
  */
 package academia;
 
-/**
- *
- * @author Leona
- */
-public class Pessoa {
-    
+public abstract class Pessoa {
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private String telefone;
+    private String email;
+
+    // Construtor
+    public Pessoa(String nome, String cpf, String endereco, String telefone, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Método abstrato (opcional) que deve ser implementado nas subclasses
+    public abstract String obterTipo(); // Exemplo: "Cliente" ou "Instrutor"
+
+    // Sobrescrita do método toString() para exibir informações básicas da pessoa
+    @Override
+    public String toString() {
+        return "Nome: " + nome + ", CPF: " + cpf + ", Telefone: " + telefone + ", Email: " + email;
+    }
 }
