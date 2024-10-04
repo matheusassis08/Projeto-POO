f9b38edf83a83e4e1715adf572f1851d6492553c
Leona <Leona@Leonardo-PC>
2024-10-03 22:21:57 -0300

    Começei a definir as funcionalidades dentro da main

--- Academia/src/academia/Academia.java
+++ Academia/src/academia/Academia.java
@@ -3,32 +3,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package academia;
+import java.util.Scanner;
 
 public class Academia {
     public static void main(String[] args) {
-        // Teste da classe Pessoa
-        System.out.println("=== Teste Pessoa ===");
-        Pessoa pessoa1 = new Cliente("Carlos Almeida", "12345678900", "Rua X, 123", "99999-8888", "carlos@email.com");
-        System.out.println(pessoa1.toString()); // Verificar se o método toString funciona
-        System.out.println("Tipo: " + pessoa1.obterTipo()); // Verificando o tipo
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1.Clientes\n2.Produtos\n3.Realizar Venda\n4.Agendamentos\n5.Relatorios\n6.Instrutor");
+        int n = scanner.nextInt();
 
-        // Testando Getters e Setters
-        pessoa1.setNome("Carlos Silva");
-        System.out.println("Nome atualizado: " + pessoa1.getNome()); // Verificando se o setter funcionou
+        switch(n){
+            case 1 -> System.out.println("1.Adicionar Cliente\n2.Alterar Cliente\n3.Apagar Cliente");
+            case 2 -> System.out.println("1.Adicionar Produto\n2.Alterar Produto\n3.Apagar Produto\n4.Verificar Estoque\n");
+            case 3 -> System.out.println();
+            case 4 -> System.out.println("1.Agendar Horário\n2.Verificar Horários\n3.Cancelar Agendamento");
+            case 5 -> System.out.println("1.Verificar Relatórios\n2.Balanços");
+            case 6 -> System.out.println("1.Gerar Ficha\n2.Alterar Ficha\n3.Exlcuir Ficha");
+            default -> System.out.println("Não existe essa opção");
+        }
 
-        // Teste da classe Cliente
-        System.out.println("\n=== Teste Cliente ===");
-        Cliente cliente1 = new Cliente("Ana Souza", "98765432100", "Rua Y, 456", "99999-7777", "ana@email.com");
-        System.out.println(cliente1.toString()); // Verificar se o toString do cliente funciona
-        cliente1.setTelefone("88888-7777");
-        System.out.println("Telefone atualizado do cliente: " + cliente1.getTelefone()); // Testando o setter
-
-        // Teste da classe Instrutor
-        System.out.println("\n=== Teste Instrutor ===");
-        Instrutor instrutor1 = new Instrutor("João Pereira", "12398745600", "Rua Z, 789", "99999-6666", "joao@email.com", "Gerente", 3500.0);
-        System.out.println(instrutor1.toString()); // Verificar se o toString do instrutor funciona
-        instrutor1.setSalario(3800.0);
-        System.out.println("Salário atualizado: " + instrutor1.getSalario()); // Verificar se o setter do salário funcionou
-        System.out.println("Tipo: " + instrutor1.obterTipo()); // Verificando o tipo
     }
 }
