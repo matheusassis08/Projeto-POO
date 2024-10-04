e66251be7606dd791c09cd74fc9ea96e5f2ef9de
matheusassis08 <matheusassis331@gmail.com>
2024-10-03 22:21:24 -0300

    Classe recepcionista com métodos

--- Academia/src/academia/Recepcionista.java
+++ Academia/src/academia/Recepcionista.java
@@ -4,10 +4,33 @@
  */
 package academia;
 
-/**
- *
- * @author Leona
- */
-public class Recepcionista {
-    
+public class Recepcionista extends Pessoa {
+    private String turno; // Turno de trabalho (manhã, tarde, noite)
+
+    // Construtor que utiliza o construtor da classe Pessoa e adiciona atributos específicos do recepcionista
+    public Recepcionista(String nome, String cpf, String endereco, String telefone, String email, String turno, double salario) {
+        super(nome, cpf, endereco, telefone, email); // Chama o construtor da superclasse Pessoa
+        this.turno = turno;
+    }
+
+    // Implementação do método abstrato
+    @Override
+    public String obterTipo() {
+        return "Recepcionista"; // Retorna o tipo como "Recepcionista"
+    }
+
+    // Getters e Setters para os atributos específicos do Recepcionista
+    public String getTurno() {
+        return turno;
+    }
+
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+
+    // Sobrescrita do método toString() para incluir turno e salário
+    @Override
+    public String toString() {
+        return "Recepcionista -> " + super.toString() + ", Turno: " + turno;
+    }
 }
