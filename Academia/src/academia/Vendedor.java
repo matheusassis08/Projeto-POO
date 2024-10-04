373802d3996580af8130d4c179b1ad86b6fe4e10
matheusassis08 <matheusassis331@gmail.com>
2024-10-03 22:59:42 -0300

    Atualizei e documentei a classe vendedor

--- Academia/src/academia/Vendedor.java
+++ Academia/src/academia/Vendedor.java
@@ -5,9 +5,38 @@
 package academia;
 
 /**
- *
- * @author Leona
+ * A classe Vendedor representa um vendedor que trabalha na academia.
+ * Ela herda os atributos e comportamentos da classe abstrata Pessoa.
  */
-public class Vendedor {
-    
+public class Vendedor extends Pessoa {
+
+    private String setor;
+
+    /**
+     * Construtor da classe Vendedor.
+     * 
+     * @param nome      O nome completo do vendedor.
+     * @param cpf       O CPF do vendedor.
+     * @param endereco  O endereço residencial do vendedor.
+     * @param telefone  O número de telefone do vendedor.
+     * @param email     O endereço de email do vendedor.
+     * @param setor     O setor onde o vendedor atua.
+     */
+    public Vendedor(String nome, String cpf, String endereco, String telefone, String email, String setor) {
+        super(nome, cpf, endereco, telefone, email);
+        this.setor = setor;
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+
+    @Override
+    public String toString() {
+        return "Vendedor -> " + super.toString() + ", Setor: " + setor;
+    }
 }
