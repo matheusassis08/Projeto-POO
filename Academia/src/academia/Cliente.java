5b218c6328a0b7a35b5d44568e2150b5effa7961
matheusassis08 <matheusassis331@gmail.com>
2024-10-03 21:26:13 -0300

    Coloquei métodos classe cliente

--- Academia/src/academia/Cliente.java
+++ Academia/src/academia/Cliente.java
@@ -4,10 +4,24 @@
  */
 package academia;
 
-/**
- *
- * @author Leona
- */
-public class Cliente {
-    
+public class Cliente extends Pessoa {
+    // Atributos específicos do Cliente podem ser adicionados aqui, se necessário.
+
+    // Construtor que utiliza o construtor da classe Pessoa
+    public Cliente(String nome, String cpf, String endereco, String telefone, String email) {
+        super(nome, cpf, endereco, telefone, email);
+    }
+
+    // Implementação do método abstrato
+    @Override
+    public String obterTipo() {
+        return "Cliente"; // Retorna o tipo como "Cliente"
+    }
+
+    // Sobrescrita do método toString() para incluir mais detalhes se necessário
+    @Override
+    public String toString() {
+        return "Cliente -> " + super.toString();
+    }
 }
+
