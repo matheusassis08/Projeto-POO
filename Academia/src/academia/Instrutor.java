b6cbd40846f1b141cc663ca61c76397f07e1cd67
matheusassis08 <matheusassis331@gmail.com>
2024-10-03 21:25:22 -0300

    Coloquei métodos classe instrutor

--- Academia/src/academia/Instrutor.java
+++ Academia/src/academia/Instrutor.java
@@ -4,10 +4,43 @@
  */
 package academia;
 
-/**
- *
- * @author Leona
- */
-public class Instrutor {
-    
+public class Instrutor extends Pessoa {
+    private String cargo;
+    private double salario;
+
+    // Construtor que utiliza o construtor da classe Pessoa e adiciona atributos específicos do instrutor
+    public Instrutor(String nome, String cpf, String endereco, String telefone, String email, String cargo, double salario) {
+        super(nome, cpf, endereco, telefone, email); // Chama o construtor da superclasse Pessoa
+        this.cargo = cargo;
+        this.salario = salario;
+    }
+
+    // Implementação do método abstrato
+    @Override
+    public String obterTipo() {
+        return "Instrutor"; // Retorna o tipo como "Instrutor"
+    }
+
+    // Getters e Setters para os atributos específicos do Instrutor
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    // Sobrescrita do método toString() para incluir cargo e salário
+    @Override
+    public String toString() {
+        return "Instrutor -> " + super.toString() + ", Cargo: " + cargo + ", Salario: " + salario;
+    }
 }
