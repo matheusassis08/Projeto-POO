3009d914f849ae54cc80664054b43f39d498181c
Leona <Leona@Leonardo-PC>
2024-10-03 23:08:02 -0300

    alterei ficha treino

--- Academia/src/academia/FichaTreino.java
+++ Academia/src/academia/FichaTreino.java
@@ -9,22 +9,75 @@ package academia;
  * @author Leona
  */
 public class FichaTreino {
-    String nomeCliente;
-    String data;
-    int numeroFicha;
-    String nomeInstrutor;
-    String listaExercicio;
-    
+    private String nomeCliente;
+    private String data;
+    private int numeroFicha;
+    private String nomeInstrutor;
+    private String listaExercicio;
+    /** Usado para iniciar um cadastro de ficha de treino para algum aluno*/
     void gerarFicha(){
         
     };
+    /** Altera os exercicios de alguma ficha já cadastrada */
     void alterarFicha(){
     
     }
+    /** Excluir alguma ficha de treino cadastrada*/
     void excluirFicha(){
     
     }
+    /** Realiza uma busca e retorna as informações de alguma ficha cadastrada*/
     void buscarFicha(){
     
     }
+
+    public FichaTreino(String nomeCliente, String data, int numeroFicha, String nomeInstrutor, String listaExercicio) {
+        this.nomeCliente = nomeCliente;
+        this.data = data;
+        this.numeroFicha = numeroFicha;
+        this.nomeInstrutor = nomeInstrutor;
+        this.listaExercicio = listaExercicio;
+    }
+    
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public int getNumeroFicha() {
+        return numeroFicha;
+    }
+
+    public void setNumeroFicha(int numeroFicha) {
+        this.numeroFicha = numeroFicha;
+    }
+
+    public String getNomeInstrutor() {
+        return nomeInstrutor;
+    }
+
+    public void setNomeInstrutor(String nomeInstrutor) {
+        this.nomeInstrutor = nomeInstrutor;
+    }
+
+    public String getListaExercicio() {
+        return listaExercicio;
+    }
+
+    public void setListaExercicio(String listaExercicio) {
+        this.listaExercicio = listaExercicio;
+    }
+    
+    
 }
